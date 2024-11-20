b2f34fb3bef9de581e1197db45359410ce00a94f
NgoVietHoang <>
2024-11-20 19:22:21 +0700

    up

--- CuoiKi/app/build.gradle.kts
+++ CuoiKi/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -32,12 +33,25 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.activity:activity:1.7.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    // Room Database
+    implementation("androidx.room:room-runtime:2.5.0")
+    annotationProcessor("androidx.room:room-compiler:2.5.0") // Sử dụng annotation processor cho Room
+
+    // MPAndroidChart để vẽ biểu đồ
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    // Glide để load ảnh
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
+    // RecyclerView để quản lý danh sách
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
 }
